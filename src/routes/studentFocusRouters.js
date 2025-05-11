6c7793922784d0ae87cc92cce55fe44e07f4d957
XuanhuiWu1103 <153825845+StartAt2023@users.noreply.github.com>
2025-05-11 14:41:04 +1000

    some changes

--- src/routes/studentFocusRouters.js
+++ src/routes/studentFocusRouters.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const cyberSecurityController = require('../controllers/studentFocusController'); 
 
-router.get('/time', cyberSecurityController.getAllComments);
+router.get('/time', cyberSecurityController.getAllStudentData);
 
 module.exports = router;
