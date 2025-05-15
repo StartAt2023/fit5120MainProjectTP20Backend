4696a2056b77f3d1198a09cff1b5552d7c843ea0
XuanhuiWu1103 <153825845+StartAt2023@users.noreply.github.com>
2025-05-15 22:32:01 +1000

    little change

--- src/routes/cyberSecurityRouters.js
+++ src/routes/cyberSecurityRouters.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const cyberSecurityController = require('../controllers/cyberSecurityController'); 
 
-router.get('/comments', cyberSecurityController.getCyberData);
+router.get('/cycberData', cyberSecurityController.getCyberData);
 
 module.exports = router;
